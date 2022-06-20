1bbffc61cd926696b3582afa4aea33d4cf94eda4
mbholt <85723341+mbholt@users.noreply.github.com>
2022-06-20 00:00:35 -0400

    fix reaction router

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -24,6 +24,9 @@ router
 router
     .route('/:thoughtId/reactions')
     .post(addReaction)
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
     .delete(deleteReaction);
 
 module.exports = router;
